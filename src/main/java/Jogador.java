2896d31a6e4f7876f32e0a43b08d5b6d8f57fbff
FabricioOliveira08 <fernandesfabricio2019@gmail.com>
2026-08-12 20:06:15 -0300

    implementa classe Jogador e ajusta encapsulamento de Personagem

--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -1,2 +1,37 @@
 public class Jogador {
+    private String nome;
+    private String personagemEscolhido;
+    private int pontuacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public String getPersonagemEscolhido() {
+        return personagemEscolhido;
+    }
+
+    public void exibirInfo() {
+
+    }
+
+    public void escolherPersonagem() {
+
+    }
+
+    public void addPontos() {
+
+    }
+
+    public void removerPontos() {
+
+    }
 }
--- src/main/java/Personagem.java
+++ src/main/java/Personagem.java
@@ -9,40 +9,20 @@ public class Personagem {
         return nivel;
     }
 
-    public void setNivel(int nivel) {
-        this.nivel = nivel;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public int getVida() {
         return vida;
     }
 
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
     public int getAtaque() {
         return ataque;
     }
 
-    public void setAtaque(int ataque) {
-        this.ataque = ataque;
-    }
-
     public int getDefesa() {
         return defesa;
-    }
-
-    public void setDefesa(int defesa) {
-        this.defesa = defesa;
     }
 
 
